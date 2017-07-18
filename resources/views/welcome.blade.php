e66406387cec70ca3deb9679e29b6a1b448a5ab2
151610175-smkassalaambandung <151610175@smkassalaambandung.sch.id>
2017-07-18 11:28:15 +0700

    Project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('content')
+<div class="contrainer">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Larapus
+                </div>
+                <div class="panel-body">
+                    Selamat Datang Di Laranilai
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

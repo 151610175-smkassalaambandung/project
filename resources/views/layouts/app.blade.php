04538e9eba17b487c0b9ef4f2f5fa90a1591a798
151610175-smkassalaambandung <151610175@smkassalaambandung.sch.id>
2017-07-19 11:32:04 +0700

    project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,15 @@
     <link href="{{asset('/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('/css/app.css')}}" rel="stylesheet">
     <link href="{{asset('/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/bootstrap.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/font-awesome.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('/css/zabuto_calendar.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('/js/gritter/css/jquery.gritter.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('/css/lineicons/style.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('/css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/style-responsive.css')}}" rel="stylesheet">
+
+    <script src="{{asset('/js/chart-master/Chart.js')}}"></script>
 
     <!-- Scripts -->
     <script>
@@ -21,25 +30,24 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
+        <header class="header black-bg">
+              <div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+              </div>
+            <!--logo start-->
+            <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            </a>
+            <!--logo end-->
+            <div class="nav notify-row" id="top_menu">
+                <!--  notification start -->
+                <ul class="nav top-menu">
+                    <!-- settings start -->
+                    <!-- inbox dropdown end -->
+                </ul>
+                <!--  notification end -->
+            </div>
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                         &nbsp;
@@ -73,8 +81,79 @@
                         @endif
                     </ul>
                 </div>
-            </div>
-        </nav>
+        </header>
+
+
+        @if(Auth::check())
+        <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu" id="nav-accordion">
+                  <p class="centered"><a href=""><img src="{{asset('/wdw.jpg')}}" class="img-circle" width="60"></a></p>
+                  <h5 class="centered">{{ Auth::user()->name }} </h5>
+                    
+                  <li class="mt">
+                      <a class="active" href="{{ url('/') }}">
+                          <i class="fa fa-dashboard"></i>
+                          <span>Notifikasi</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-user"></i>
+                          <span>Siswa</span>
+                      </a>
+                  </li>
+                  @role('admin')
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-cogs"></i>
+                          <span>Kelas</span>
+                      </a>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-book"></i>
+                          <span>Pelajaran</span>
+                      </a>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-tasks"></i>
+                          <span>Nilai</span>
+                      </a>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-user"></i>
+                          <span>Guru</span>
+                      </a>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class=" fa fa-bar-chart-o"></i>
+                          <span>Admin</span>
+                      </a>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class=" fa fa-bar-chart-o"></i>
+                          <span>Laporan</span>
+                      </a>
+                  </li>
+                  @endrole
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
+        @endif
+
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
 
         @yield('content')
     </div>
@@ -83,5 +162,77 @@
     <script src="{{asset('/js/app.js')}}"></script>
     <!-- <script src="{{asset('/js/bootstrap.min.js')}}"></script> -->
     <script src="{{asset('/js/jquery-3.2.1.min.js')}}"></script>
+    <script src="{{asset('/js/jquery.js')}}"></script>
+    <script src="{{asset('/js/jquery-1.8.3.min.js')}}"></script>
+    <script src="{{asset('/js/bootstrap.min.js')}}"></script>
+    <script class="include" type="text/javascript" src="{{asset('/js/jquery.dcjqaccordion.2.7.js')}}"></script>
+    <script src="{{asset('/js/jquery.scrollTo.min.js')}}"></script>
+    <script src="{{asset('/js/jquery.nicescroll.js')}}" type="text/javascript"></script>
+    <script src="{{asset('/js/jquery.sparkline.js')}}"></script>
+
+
+    <!--common script for all pages-->
+    <script src="{{asset('/js/common-scripts.js')}}"></script>
+    
+    <script type="text/javascript" src="{{asset('/js/gritter/js/jquery.gritter.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/js/gritter-conf.js')}}"></script>
+
+    <!--script for this page-->
+    <script src="{{asset('/js/sparkline-chart.js')}}"></script>    
+    <script src="{{asset('/js/zabuto_calendar.js')}}"></script>    
+    
+    <script type="text/javascript">
+        $(document).ready(function () {
+        var unique_id = $.gritter.add({
+            // (string | mandatory) the heading of the notification
+            title: 'LARANILAI',
+            // (string | mandatory) the text inside the notification
+            text: 'Aplikasi ini ditujukan untuk pengolahan nilai siswa',
+            // (bool | optional) if you want it to fade out on its own or just sit there
+            sticky: true,
+            // (int | optional) the time you want it to be alive for before fading out
+            time: '',
+            // (string | optional) the class name you want to apply to that specific message
+            class_name: 'my-sticky-class'
+        });
+
+        return false;
+        });
+    </script>
+    
+    <script type="application/javascript">
+        $(document).ready(function () {
+            $("#date-popover").popover({html: true, trigger: "manual"});
+            $("#date-popover").hide();
+            $("#date-popover").click(function (e) {
+                $(this).hide();
+            });
+        
+            $("#my-calendar").zabuto_calendar({
+                action: function () {
+                    return myDateFunction(this.id, false);
+                },
+                action_nav: function () {
+                    return myNavFunction(this.id);
+                },
+                ajax: {
+                    url: "show_data.php?action=1",
+                    modal: true
+                },
+                legend: [
+                    {type: "text", label: "Special event", badge: "00"},
+                    {type: "block", label: "Regular event", }
+                ]
+            });
+        });
+        
+        
+        function myNavFunction(id) {
+            $("#date-popover").hide();
+            var nav = $("#" + id).data("navigation");
+            var to = $("#" + id).data("to");
+            console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
+        }
+    </script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    Larapus
+                    Laranilai
                 </div>
                 <div class="panel-body">
                     Selamat Datang Di Laranilai
